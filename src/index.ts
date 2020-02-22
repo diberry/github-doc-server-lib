0cd28b4912b77b31c431ff6d2c3b6f78f185dbeb
Dina Berry <diberry@microsoft.com>
2020-02-22 02:31:11 +0000

    typescript config files work as expected

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// testing for any
+
+export class EsLintTest {
+    credentials: any;
+
+    myparam;
+
+    constructor(credentials: any) {
+      this.credentials = credentials;
+
+    }
+    thisTest(): any {
+      return 2+2;
+    }
+  }
